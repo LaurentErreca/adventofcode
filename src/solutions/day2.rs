95a00ca878a409e5eb92fdf21a887678ffb60555
Laurent Erreca <laurent.erreca@cfm.fr>
2022-07-01 13:20:55 +0200

    Complete day 2 part 2

--- src/solutions/day2.rs
+++ src/solutions/day2.rs
@@ -1,9 +1,13 @@
 
+fn apply_forward(hpos: &i32, aim: &i32, value: &i32, depth: &i32) -> (i32, i32) {
+    return (hpos + value, depth + (aim * value));
+}
+
 pub fn solve(part: u8, input: &String) -> String {
     let mut hpos: i32 = 0;
     let mut depth: i32 = 0;
+    let mut aim: i32 = 0;
     let vecstr: Vec<&str> = input.lines().collect();
-    //let vecint: Vec<i32> = vecstr.into_iter().map(|x| x.parse().unwrap()).collect();
     let vecstr_iter = vecstr.iter();
     if part == 1 {
         for command in vecstr_iter {
@@ -11,7 +15,6 @@ pub fn solve(part: u8, input: &String) -> String {
             let command_tuple = (split.next().unwrap(), split.next().unwrap());
             let action = command_tuple.0;
             let value: i32 = command_tuple.1.parse().unwrap();
-            //println!("action : {} - value : {}", action, value);
             match action {
                 "up" => depth -= value,
                 "down" => depth += value,
@@ -23,7 +26,20 @@ pub fn solve(part: u8, input: &String) -> String {
         return (depth * hpos).to_string();
     }
     if part == 2 {
-        return String::from("part must be 1");
+        for command in vecstr_iter {
+            let mut split = command.split(" ");
+            let command_tuple = (split.next().unwrap(), split.next().unwrap());
+            let action = command_tuple.0;
+            let value: i32 = command_tuple.1.parse().unwrap();
+            match action {
+                "up" => aim -= value,
+                "down" => aim += value,
+                "forward" => (hpos, depth) = apply_forward(&hpos, &aim, &value, &depth), //hpos += value,// depth = aim * value,
+                _ => println!("Wrong action"),
+            }
+        }
+        println!("depth : {} - hpos : {}", depth, hpos);
+        return (depth * hpos).to_string();
     }
     return String::from("part must be 1");
 }
